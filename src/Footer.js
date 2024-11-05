9798c1c62c3cd8604d60072624ea68d1d1325e00
andreamorinigo7 <morinigoandrea7@gmail.com>
2024-11-05 15:12:41 -0500

    added folder into git

---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      <a href="">Open source code</a>, by <a href="">Andrea Morinigo</a>
+    </footer>
+  );
+}
